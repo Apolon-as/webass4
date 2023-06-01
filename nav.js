e2d1b18570018a45767212baad6dd421dc3f45f8
killayera <130810825+killayera@users.noreply.github.com>
2023-06-01 19:58:28 +0600

    added js, and modified css
    Added javascript scroll, and modified menu

--- nav.js
+++ nav.js
@@ -1,21 +1,22 @@
 const openMenu = () => {
-    document.querySelector ( selectors: '.backdrop') .cLassName = 'backdrop active';
-    document.querySelector ( selectors: 'aside').className = 'active';
+    document.querySelector('.backdrop').className = 'backdrop active';
+    document.querySelector('aside').className = 'active';
 }
 
 const closeMenu = () => {
-    document. querySelector ( selectors: '.backdrop') .className = 'backdrop';
-    document.querySelector ( selectors: 'aside').className ='';
+    document.querySelector('.backdrop').className = 'backdrop';
+    document.querySelector('aside').className = '';
 }
 
-document. getElementById( elementId: 'menuBtn').onclick = e => {
-e.preventDefault () ;
-openMenu ();
+document.getElementById('menuBtn').onclick = e => {
+    e.preventDefault();
+    openMenu();
 }
 
-document.querySelector ( selectors: 'aside button.close').onclick = e => {
+document.querySelector('aside button.close').onclick = e => {
     closeMenu();
 }
-    document.querySelector( selectors: '.backdrop').onclick = e => {
+
+document.querySelector('.backdrop').onclick = e => {
     closeMenu();
 }
